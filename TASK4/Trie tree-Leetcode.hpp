381cc5476581c723f132798a0a7a39d3ad5a935c
Penkace <penkaceunique@foxmail.com>
2019-03-09 14:54:08 +0800

    Create Trie tree-Leetcode.hpp

---
+++ TASK4/Trie tree-Leetcode.hpp
@@ -0,0 +1,56 @@
+struct TrieNode {
+    TrieNode() {
+        for (int i=0;i<256;i++) {
+            mData[i] = NULL;
+        }
+        mWord = false;
+    }
+    
+    TrieNode *mData[256];
+    bool mWord;
+};
+
+
+class Trie {
+public:
+    
+    /** Initialize your data structure here. */
+    Trie() {
+        mRoot = new TrieNode();
+    }
+    
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        TrieNode *aNode = mRoot;
+        int l = word.size();
+        for (int i=0;i<l;i++) {
+            if (aNode->mData[word[i]-'a'] == 0) {
+                aNode->mData[word[i]-'a'] = new TrieNode();
+            }
+            aNode = aNode->mData[word[i]-'a'];
+        }
+        aNode->mWord = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        TrieNode *aNode = mRoot;
+        int l = word.size();
+        for (int i=0;i<l&& aNode;i++) {
+            aNode = aNode->mData[word[i]-'a'];
+        }
+        return aNode&&aNode->mWord;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        TrieNode *aNode = mRoot;
+        int l = prefix.size();
+        for (int i=0;i<l&& aNode;i++) {
+            aNode = aNode->mData[prefix[i]-'a'];
+        }
+        return aNode;
+    }
+    
+    TrieNode *mRoot;
+};
