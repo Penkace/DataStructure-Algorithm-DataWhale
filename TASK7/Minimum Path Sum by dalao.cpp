38fdd25503586114113ff659dd9c4612defb0e5a
Penkace <penkaceunique@foxmail.com>
2019-03-16 20:44:38 +0800

    Update Minimum Path Sum by dalao.cpp

--- TASK7/Minimum Path Sum by dalao.cpp
+++ TASK7/Minimum Path Sum by dalao.cpp
@@ -15,3 +15,21 @@ public:
         return grid[grid.size()-1][grid[grid.size()-1].size()-1];
     }
 };
+
+// 做了一点优化，并没有太大的提高了
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int rend = grid.size();
+        int cend = grid[0].size();
+        
+        for(int i=1;i<rend;i++) grid[i][0]+=grid[i-1][0];
+        for(int j=1;j<cend;j++) grid[0][j]+=grid[0][j-1];
+        for(int i=1;i<rend;i++){
+            for(int j=1;j<cend;j++){
+                grid[i][j]+=min(grid[i-1][j],grid[i][j-1]);
+            }
+        }
+        return grid[rend-1][cend-1];
+    }
+};
